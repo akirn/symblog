45ed0f56774418cc384216483c75fa81b2ee4c91
Andreas Kirn <akirn@switcheye.com>
2013-10-08 22:52:51 -0700

    updated contactAction to process form

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -17,9 +17,27 @@ class PageController extends Controller
         return $this->render('BloggerBlogBundle:Page:about.html.twig');
     }
 
-    public function contactAction()
+	public function contactAction()
 	{
-	    return $this->render('BloggerBlogBundle:Page:contact.html.twig');
+	    $enquiry = new Enquiry();
+	    $form = $this->createForm(new EnquiryType(), $enquiry);
+
+	    $request = $this->getRequest();
+	    if ($request->getMethod() == 'POST') {
+	        $form->bindRequest($request);
+
+	        if ($form->isValid()) {
+	            // Perform some action, such as sending an email
+
+	            // Redirect - This is important to prevent users re-posting
+	            // the form if they refresh the page
+	            return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+	        }
+	    }
+
+	    return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+	        'form' => $form->createView()
+	    ));
 	}
 }
 
