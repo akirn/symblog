c875129c8fa71075d157a1b16be1c8b7adf791a3
Andreas Kirn <akirn@switcheye.com>
2013-10-09 00:07:30 -0700

    added validators

--- src/Blogger/BlogBundle/Entity/Enquiry.php
+++ src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -3,6 +3,12 @@
 
 namespace Blogger\BlogBundle\Entity;
 
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\MinLength;
+use Symfony\Component\Validator\Constraints\MaxLength;
+
 class Enquiry
 {
     protected $name;
@@ -52,4 +58,16 @@ class Enquiry
     {
         $this->body = $body;
     }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('name', new NotBlank());
+
+        $metadata->addPropertyConstraint('email', new Email());
+
+        $metadata->addPropertyConstraint('subject', new NotBlank());
+        $metadata->addPropertyConstraint('subject', new MaxLength(50));
+
+        $metadata->addPropertyConstraint('body', new MinLength(50));
+    }    
 }
