2ccf58b291993ace33b7a3da8ec8c91f003e507d
Andreas Kirn <akirn@switcheye.com>
2013-10-12 00:06:25 -0700

    enable truncation in getBlog

--- src/Blogger/BlogBundle/Entity/Blog.php
+++ src/Blogger/BlogBundle/Entity/Blog.php
@@ -144,9 +144,13 @@ class Blog
      *
      * @return string 
      */
-    public function getBlog()
+    public function getBlog($length = null)
     {
-        return $this->blog;
+        if (false === is_null($length) && $length > 0) {
+            return substr($this->blog, 0, $length);
+        } else {
+            return $this->blog;
+        }
     }
 
     /**
