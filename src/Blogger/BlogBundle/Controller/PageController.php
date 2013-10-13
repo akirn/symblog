4c1120a0e9bbf9154b0abd16429ae73776be4123
Andreas Kirn <akirn@switcheye.com>
2013-10-13 09:55:44 -0700

    added comments

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -14,12 +14,8 @@ class PageController extends Controller
         $em = $this->getDoctrine()
                    ->getManager();
 
-        $blogs = $em->createQueryBuilder()
-                    ->select('b')
-                    ->from('BloggerBlogBundle:Blog', 'b')
-                    ->addOrderBy('b.created', 'DESC')
-                    ->getQuery()
-                    ->getResult();
+        $blogs = $em->getRepository('BloggerBlogBundle:Blog')
+                    ->getLatestBlogs();
 
         return $this->render('BloggerBlogBundle:Page:index.html.twig', array(
             'blogs' => $blogs
