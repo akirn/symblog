dac13cb57af077dab3aab379881cc14923df3e9f
Andreas Kirn <akirn@switcheye.com>
2013-10-13 21:03:54 -0700

    slugify urls

--- src/Blogger/BlogBundle/Entity/Blog.php
+++ src/Blogger/BlogBundle/Entity/Blog.php
@@ -18,6 +18,11 @@ class Blog
      */
     protected $comments;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $slug;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
@@ -94,6 +99,8 @@ class Blog
     public function setTitle($title)
     {
         $this->title = $title;
+
+        $this->setSlug($this->title);
     
         return $this;
     }
@@ -286,5 +293,46 @@ class Blog
     public function __toString()
     {
         return $this->getTitle();
+    }
+
+    public function setSlug($slug)
+    {
+        $this->slug = $this->slugify($slug);
+    }
+
+    public function slugify($text)
+    {
+        // replace non letter or digits by -
+        $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // transliterate
+        if (function_exists('iconv')) {
+            $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        }
+
+        // lowercase
+        $text = strtolower($text);
+
+        // remove unwanted characters
+        $text = preg_replace('#[^-\w]+#', '', $text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
     }
 }
