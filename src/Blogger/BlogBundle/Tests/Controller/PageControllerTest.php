8c6fb916f9da4ce3a29451e328dd543d4e94bcb8
Andreas Kirn <akirn@switcheye.com>
2013-10-15 19:06:52 -0700

    initial commit

---
+++ src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Blogger\BlogBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PageControllerTest extends WebTestCase
+{
+    public function testAbout()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/about');
+
+        $this->assertEquals(1, $crawler->filter('h1:contains("About symblog")')->count());
+    }
+
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        // Check there are some blog entries on the page
+        $this->assertTrue($crawler->filter('article.blog')->count() > 0);
+
+        // Find the first link, get the title, ensure this is loaded on the next page
+        $blogLink   = $crawler->filter('article.blog h2 a')->first();
+        $blogTitle  = $blogLink->text();
+        $crawler    = $client->click($blogLink->link());
+
+        // Check the h2 has the blog title in it
+        $this->assertEquals(1, $crawler->filter('h2:contains("' . $blogTitle .'")')->count());
+    }
+
+    public function testContact()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/contact');
+
+        $this->assertEquals(1, $crawler->filter('h1:contains("Contact symblog")')->count());
+
+        // Select based on button value, or id or name for buttons
+        $form = $crawler->selectButton('Submit')->form();
+
+        $form['contact[name]']       = 'name';
+        $form['contact[email]']      = 'email@email.com';
+        $form['contact[subject]']    = 'Subject';
+        $form['contact[body]']       = 'The comment body must be at least 50 characters long as there is a validation constrain on the Enquiry entity';
+
+        $crawler = $client->submit($form);
+        
+        // Check email has been sent
+        if ($profile = $client->getProfile()) {
+            $swiftMailerProfiler = $profile->getCollector('swiftmailer');
+
+            // Only 1 message should have been sent
+            $this->assertEquals(1, $swiftMailerProfiler->getMessageCount());
+
+            // Get the first message
+            $messages = $swiftMailerProfiler->getMessages();
+            $message  = array_shift($messages);
+
+            $symblogEmail = $client->getContainer()->getParameter('blogger_blog.emails.contact_email');
+            // Check message is being sent to correct address
+            $this->assertArrayHasKey($symblogEmail, $message->getTo());
+        }
+
+        // Need to follow redirect
+        $crawler = $client->followRedirect();
+
+        $this->assertTrue($crawler->filter('.blogger-notice:contains("Your contact enquiry was successfully sent. Thank you!")')->count() > 0);
+    }
+
+    public function testAddBlogComment()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/1/a-day-with-symfony');
+
+        $this->assertEquals(1, $crawler->filter('h2:contains("A day with Symfony2")')->count());
+
+        // Select based on button value, or id or name for buttons
+        $form = $crawler->selectButton('Submit')->form();
+
+        $crawler = $client->submit($form, array(
+            'blogger_blogbundle_comment[user]'          => 'name',
+            'blogger_blogbundle_comment[comment]'       => 'comment',
+        ));
+
+        // Need to follow redirect
+        $crawler = $client->followRedirect();
+
+        // Check comment is now displaying on page, as the last entry. This ensure comments
+        // are posted in order of oldest to newest
+        $articleCrawler = $crawler->filter('section .previous-comments article')->last();
+
+        $this->assertEquals('name', $articleCrawler->filter('header span.highlight')->text());
+        $this->assertEquals('comment', $articleCrawler->filter('p')->last()->text());
+
+        // Check the sidebar to ensure latest comments are display and there is 10 of them
+
+        $this->assertEquals(10, $crawler->filter('aside.sidebar section')->last()
+                                        ->filter('article')->count()
+        );
+
+        $this->assertEquals('name', $crawler->filter('aside.sidebar section')->last()
+                                            ->filter('article')->first()
+                                            ->filter('header span.highlight')->text()
+        );
+    }
+}
